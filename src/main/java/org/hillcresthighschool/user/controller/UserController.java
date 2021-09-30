0aa92c1b0ee98eff78d63c26f6670a430b66d7e8
Sathya Tadinada <57514020+SathyaTadinada@users.noreply.github.com>
2021-09-30 07:11:06 -0600

    Removed unused import statement from UserController

--- src/main/java/org/hillcresthighschool/user/controller/UserController.java
+++ src/main/java/org/hillcresthighschool/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package org.hillcresthighschool.user.controller;
 
 import org.hillcresthighschool.user.dto.UserDTO;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
