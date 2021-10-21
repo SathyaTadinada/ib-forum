0bb99b6c9d3f4c8e29916fb9c83e3ebc24fe976d
michaelchen78 <michaelochen78@gmail.com>
2021-10-20 18:44:45 -0600

    added category, question, and user type tables
    added unique key constraints

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,54 @@ CREATE TABLE users (
     password_hash VARCHAR(100) NOT NULL,
     salt VARCHAR(100) NOT NULL
 );
+
+CREATE UNIQUE INDEX U_users_email ON users
+(
+    email
+);
+CREATE UNIQUE INDEX U_users_username ON users
+(
+    username
+);
+
+
+DROP TABLE IF EXISTS category;
+
+CREATE TABLE category(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(35) NOT NULL,
+    archived BIT DEFAULT 0 NOT NULL,
+    school_year VARCHAR(9) NOT NULL
+);
+
+CREATE UNIQUE INDEX U_category_name_school_year ON category
+(
+    name, school_year
+);
+
+DROP TABLE IF EXISTS question;
+
+CREATE TABLE question(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    text VARCHAR(200) NOT NULL,
+    date TIMESTAMP NOT NULL,
+    anonymous BIT DEFAULT 0 NOT NULL,
+    approved_by INT,
+    likes INT DEFAULT 0 NOT NULL
+
+);
+
+DROP TABLE IF EXISTS user_type;
+
+CREATE TABLE user_type(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL
+);
+
+CREATE UNIQUE INDEX U_user_type_name ON user_type
+(
+    name
+);
+
+
